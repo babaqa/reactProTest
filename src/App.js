e697fde017d32d1e6b3e42284f7e00113dbc5e45
Arhua <a0987837233@gmail.com>
2021-02-24 14:21:39 +0800

    https

--- src/App.js
+++ src/App.js
@@ -25,7 +25,8 @@ function App() {
   const [Theme, setTheme] = useReducer(reducer, themes.defaultTheme);
   // dev : http://openauth.react.dev.1966.org.tw:20025
   // uat : http://openauth.react.1966.org.tw:20026 
-  const [APIUrl, setAPIUrl] = useState("http://openauth.react.dev.1966.org.tw:20025/api/")//useState("http://openauth.1966.org.tw/api/");
+  const [APIUrl, setAPIUrl] = useState("https://pingtungdev.1966.org.tw/api/")//useState("http://openauth.1966.org.tw/api/");
+  const [APIFileUrl, setAPIFileUrl] = useState("https://pingtungdev.1966.org.tw/")//useState("http://openauth.1966.org.tw/");
   const [APIAppKey, setAPIAppKey] = useState("openauth");
   const [Value, Switch, Open, Close] = useSwitch();//控制重新渲染路由
   const [Collapse, setCollapse] = useState(false); // 控制768以上畫面，左側欄收合情況
@@ -33,7 +34,7 @@ function App() {
   return (
     <>
       <DndProvider backend={HTML5Backend}>
-        < Context.Provider value={{ APIUrl, APIAppKey, Theme, setTheme, Switch, Collapse, setCollapse }}>
+        < Context.Provider value={{ APIUrl, APIFileUrl, APIAppKey, Theme, setTheme, Switch, Collapse, setCollapse }}>
           <ContextContainer />
         </Context.Provider>
       </DndProvider>
